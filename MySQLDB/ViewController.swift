9f581908424d54e3fbfa1898f32b96ebbe870f98
GuillaumeLauzier <55703540+GuillaumeLauzier@users.noreply.github.com>
2019-12-04 19:33:43 +0100

    changes affected

--- MySQLDB/ViewController.swift
+++ MySQLDB/ViewController.swift
@@ -20,27 +20,27 @@ class ViewController: UIViewController {
     
     // the button action fonction
     @IBAction func uploadData(_ sender: Any) {
-        let url = URL(string: "https://rexiagroup.com/webservices/php/createUser.php")
+        let url = URL(string: "https://rexiagroup.com/webservices/php/createUser2.php")
         // locahost MAMP - change to point to your database server
         
         var request = URLRequest(url: url!)
         request.httpMethod = "POST"
         
-        var dataString = "secretWord=765432"
+        var dataString = "password=765432"
         // starting POST string with a secretWord
         
         // the POST string has entries separated by &
         
-        dataString = dataString + "&Email=\(Email.text!)" // add items as name and value
-        dataString = dataString + "&firstName=\(firstName.text!)"
-        dataString = dataString + "&lastName=\(lastName.text!)"
+        dataString = dataString + "&email=\(Email.text!)" // add items as name and value
+        dataString = dataString + "&firstname=\(firstName.text!)"
+        dataString = dataString + "&lastname=\(lastName.text!)"
         
         // convert the post string to utf8 format
         
         let dataD = dataString.data(using: .utf8) // convert to utf8 string
         
-        do
-        {
+        //do
+        //{
             
             // the upload task, uploadJob, is defined here
             
@@ -51,10 +51,13 @@ class ViewController: UIViewController {
                 if error != nil {
                     
                     // display an alert if there is an error inside the DispatchQueue.main.async
+                    print ("================")
+                    print (error!.localizedDescription)
+                    print ("================")
                     
                     DispatchQueue.main.async
                         {
-                            let alert = UIAlertController(title: "Upload Didn't Work?", message: "Looks like the connection to the server didn't work.  Do you have Internet access?", preferredStyle: .alert)
+                            let alert = UIAlertController(title: "Upload Didn't Work?", message: "Looks like the connection to the server didn't work.  Do you have Internet access?\n\(error!.localizedDescription)", preferredStyle: .alert)
                             alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
                             self.present(alert, animated: true, completion: nil)
                     }
@@ -65,7 +68,7 @@ class ViewController: UIViewController {
                         
                         let returnedData = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue) // Response from web server hosting the database
                         
-                        if returnedData == "1" // insert into database worked
+                        if returnedData?.substring(to: 1) == "1" // insert into database worked
                         {
                             
                             // display an alert if no error and database insert worked (return = 1) inside the DispatchQueue.main.async
@@ -93,7 +96,7 @@ class ViewController: UIViewController {
                 }
             }
             uploadJob.resume()
-        }
+        //}
     }
     
     override func didReceiveMemoryWarning()
