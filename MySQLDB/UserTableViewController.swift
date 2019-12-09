8e1573f96c2c2cc21a1e5d6a6e04ff870892d541
GuillaumeLauzier <55703540+GuillaumeLauzier@users.noreply.github.com>
2019-12-09 21:06:04 +0100

    updates

---
+++ MySQLDB/UserTableViewController.swift
@@ -0,0 +1,169 @@
+//
+//  UserTableViewController.swift
+//  MySQLDB
+//
+//  Created by ifage-user on 09.12.19.
+//  Copyright © 2019 ifage-user. All rights reserved.
+//
+
+import UIKit
+
+    struct User: Codable {
+        var admin:UInt
+        var email:String
+        var firstname:String
+        var lastname:String
+    }
+
+class UserTableViewController: UITableViewController {
+    
+    private let usersURL = "https://www.rexiagroup.com/webservices/php/selectUsers.php"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        dataOfJson(url: self.usersURL)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    func dataOfJson(url: String) {
+        let session = URLSession.shared
+        let url = URL(string: url)!
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            if error != nil || data == nil {
+                print("Client error!")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                print("Server error!")
+                return
+            }
+            
+            guard let mime = response.mimeType, mime == "text/html" else {
+                print("Wrong MIME type!")
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print(json)
+            } catch {
+                print("JSON error: \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
+    }
+    
+    /*
+     
+     struct Response: Codable
+     {
+     struct User: Codable {
+     var email:String
+     var firstname:String
+     var lastname:String
+     }
+     
+     var users:[User]
+     }
+     
+     let json = try? JSONSerialization.jsonObject(with: data, options: [])
+     
+     if let recipe = json as? [String: Any] {
+     if let yield = recipe["yield"] as? Int {
+     recipeObject.yield = yield
+     }
+     }
+     
+     struct User: Codable {
+     var first_name:String
+     var last_name:String
+     var country:String
+     }
+     
+     {
+     "first_name": "John",
+     "last_name": "Doe",
+     "country": "United Kingdom"
+     }
+     
+     */
+
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
